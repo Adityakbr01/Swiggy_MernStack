72d0aa887bdee04c1aa32da50a3159f7a4a69df1
Adityakbr01 <aditykbr01@gmail.com>
2025-05-12 20:25:17 +0530

    FRontend : -

--- Client/src/Socket/socket.ts
+++ Client/src/Socket/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000",{
+const socket = io("https://swiggy-mernstack.onrender.com/",{
     autoConnect: true,
   reconnection: true,
 }); // replace with your backend URL
--- Client/src/utils/_Constant.ts
+++ Client/src/utils/_Constant.ts
@@ -1,1 +1,1 @@
- export const RootUrl = "http://localhost:5000/api/v1"
+ export const RootUrl = "https://swiggy-mernstack.onrender.com/api/v1"
